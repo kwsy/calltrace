f4768fe20b488960ba6ba0514d903bcd6748364a
张东升 <zhangdongsheng@liepin.com>
2021-05-13 17:43:05 +0800

    修改模块

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pycalltrace',
-    version='0.1',
+    version='0.2',
     url='https://github.com/kwsy/calltrace',
     license='BSD',
     author='dongsheng zhang',
