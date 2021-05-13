6ae5b4818a3f39fd5049633661660d9a28ba5209
张东升 <zhangdongsheng@liepin.com>
2021-05-13 17:38:56 +0800

    修改setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='dongsheng zhang',
     author_email='xigongda200608@163.com',
     description='record and trace the function call chain',
-    py_modules= ['calltrace',],
+    py_modules= ['pycalltrace',],
     zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
