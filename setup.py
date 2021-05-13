0a46f46f4f1b85ce0a215e5a2695586c1b4bd3fc
张东升 <zhangdongsheng@liepin.com>
2021-05-13 16:58:00 +0800

    init

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='calltrace',
+    version='0.1',
+    url='https://github.com/kwsy/calltrace',
+    license='BSD',
+    author='dongsheng zhang',
+    author_email='xigongda200608@163.com',
+    description='record and trace the function call chain',
+    py_modules= ['calltrace',],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
---
+++ test.py
@@ -0,0 +1,28 @@
+from calltrace import CallTrace
+
+def func1():
+    print('ok')
+
+def func2():
+    func1()
+
+def func3():
+    func2()
+
+def func4():
+    func3()
+
+def file_filter(filename):
+    if filename.find('calc') != -1:
+        return True
+    return False
+
+
+with CallTrace(filter_func=lambda x: x.find('calc') == -1):
+    func4()
+
+# call_trace = CallTrace(filter_func=lambda x: x.find('calc') == -1)
+# call_trace.start()
+# func4()
+# call_trace.stop()
+# call_trace.output()
